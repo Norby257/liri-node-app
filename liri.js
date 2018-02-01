6811e44407adc48c6622efae7e3b149a6f54089d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-01-31 18:10:58 -0600

    refactored variables, added log.txt file

--- liri.js
+++ liri.js
@@ -1,48 +1,57 @@
 require("dotenv").config();
 
-var keys = require('./keys.js');
-console.log('keys.js');
 
-var spotify = new spotify(keys.spotify);
-var client = new twitter(keys.twitter);
+//consts for my project to work
+const keys = require('./keys.js');
+console.log('keys.js');
+const spotifyApi = require('node-spotify-api');
+const twitterApi = require('twitter');
+const request = require('request');
+
+
+//which file do the below vars go into?
+//skipping to omdb for now
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
 
 //takes in command line arguments as string
 
-var userInput = process.argv;
+var userInput = process.argv[2];
 
 //get string position in array of commands 
 //order of commands is node liri.js ______ thing here
-/* node liri.js my-tweets 
-node liri.js spotify-this-song
-node liri.js movie-this 
-node liri.js do-what-it-says 
-*/ 
- 
-var userInputReq = inputString[2];
-// vars for spotify? twitter? 
 
+ 
+//this variable holds the spotify /twitter /movie API 
+var userSearch = userInput[3];
 
 //switch statement -based on command, we run the corresponding request 
+//debug this so that it works 
+//see YOUR calculator solution 
 
 switch(userInput) {
     case "my-tweets":
-        console.log("my-tweets");
-        //more steps to be added 
+        console.log(userInput);
+        //fix this below 
+        // client.get('/norbyfirebase2', function(error, tweets, response) {
+
+        //     console.log(tweets);
+        //  });
         break;
     
     case "spotify-this-song": 
-        console.log("spotify-this-song");
+        console.log(userInput);
         //more steps to be added
         break;
 
 
     case "movie-this":
-        console.log("movie-this");
+        console.log(userInput);
         //more steps to be added 
         break;
 
     case ("do-what-it-says"):
-        console.log("do-what-it-says");
+        console.log(userInput);
         //more steps to be added 
         break;
 
@@ -51,6 +60,6 @@ switch(userInput) {
 
 
 
-}
+
 
 
