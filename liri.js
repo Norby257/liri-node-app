5de65c645c5b64c1364e9e25c0e1522d446299aa
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-04 15:29:26 -0600

    added functioning spotify command

--- liri.js
+++ liri.js
@@ -18,7 +18,7 @@ var client = new Twitter(keys.twitter);
 //takes in command line arguments as string
 
 var userCommand = process.argv[2];
-//how can we make this take more than one word titles?
+//how can we make this take more than one word titles? what if I have a verbose song or movie title 
 var userSearch = process.argv.slice(3);
 
 //get string position in array of commands 
@@ -37,6 +37,12 @@ switch(userCommand) {
          
             // using tweets.length so i don't have to make 20 tweets, and not get an error 
             //lets see how to arrange them in order for when they were created. so that way it's OLDEST TO NEWEST. 
+            //pseudo code this 
+            //set result of loop to array (or an object)?
+            //step thru array, and sort it according to created date
+            //display the correctly sorted tweets 
+            //moment js will prolly be really helpful here 
+
                 for (let i = 0; i < tweets.length; i++) {
                 console.log(`At this time,  ${tweets[i].created_at}`);
                 console.log(`Here's what you tweeted: ${tweets[i].text}`);
@@ -51,12 +57,25 @@ switch(userCommand) {
     case "spotify-this-song": 
         console.log(userCommand);
         console.log(userSearch);
-       spotify.search({ type: `track`, query: `${userSearch}`}, function(err, data){
+       spotify.search({ type: `track`, query: `${userSearch}`}, function(err, data, response){
            if (err) {
                console.log(`Error occured ${err}`);
                return;
            }
-           console.log(data);
+            else {
+                //just doing some testing so i can determine the exact array index from the object(s) etc 
+               let artistName = (data.tracks.items[0].artists[0].name);
+               let songName = (data.tracks.items[0].name);
+               let previewUrl = (data.tracks.items[0].preview_url);
+               let albumName = (data.tracks.items[0].album.name);
+               console.log(`Artist: ${artistName}`);
+               console.log(`Song title: ${songName}`);
+                console.log(`Click here to check out the song! ${previewUrl}`);
+                console.log(`Song is from this dope album: ${albumName}`);
+                // let songData = data.artists[0].name;
+                // console.log(songData);
+                // console.log(response.tracks.items[0]);
+            }
 
        });
         break;
